fc3563cb8a6ff532b3bb272659e427af8e8ccf84
makwalam <makwala7@gmail.com>
2023-08-25 15:32:23 -0700

    makwala

---
+++ proud.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * print_hello - Prints "Hello, Betty!" to the standard output
+ */
+
+void print_hello(void)
+{
+    printf("Hello, Betty!\n");
+}
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+    print_hello();
+    return (0);
+}
